493d482325701f6259b76d37470583eff570f560
Dbeidels <dbeidels@macalester.edu>
2022-09-22 11:22:05 -0500

    completed SecondMax

--- Week1/SecondMax/Program.cs
+++ Week1/SecondMax/Program.cs
@@ -3,7 +3,42 @@
 class Program{
     static void Main(string[] args){
         MaxSecond sec = new MaxSecond();
-        int e = sec.FindSecond(1,54,13);
-        Console.WriteLine(e);
+        Menu(sec);
+    }
+    public static void Menu(MaxSecond number){
+        bool test =true;
+        while(test){
+            Console.WriteLine("Would you like to use this program.[y/n]");
+            string answer = Console.ReadLine();
+            if(answer == "y"){
+                Console.WriteLine("Enter three numbers of different values");
+                string input = Console.ReadLine();
+                string[] str = input.Split();
+                if(str.Length == 3){
+                    List<int> i = new List<int>();
+                    int digit;
+                    foreach(string s in str){
+                        if(Int32.TryParse(s, out digit)){
+                            i.Add(digit);
+                        }
+                        else{
+                            Console.WriteLine("Please enter three Numbers of different values");
+                            break;
+                        }
+                    }
+                    Console.WriteLine(number.FindSecond(i[0],i[1],i[2]));
+                }
+                else{
+                    Console.WriteLine("Please enter Three numbers of different values");
+                }
+                
+            }
+            else if(answer == "n"){
+                test = false;
+            }
+            else{
+                Console.WriteLine("Please enter a valid answer.");
+            }
+        }
     }
 }
